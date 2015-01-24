472312e540a5bdbdd2ed731025cb1fb59cfd1ae8
mccabe19961 <brianmccabe96@gmail.com>
2015-01-24 20:20:26 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Brian McCabe
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass {
+
+//add main method
+    public static void main(String[] args) {
+        
+//print the statement
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-B--R--M--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Over winter break, I went to California to visit some old friends.");
+        System.out.println("I improved in surfing quite a bit.");
+        
+        
+    }
+    
+    
+    
+}
